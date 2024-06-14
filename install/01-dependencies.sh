b7485e2b74effa527b983ac6acdd12cdb8666614
Ivan Leonov <ivanleonov89@gmail.com>
2024-06-15 00:04:56 +0300

    Removes unused install scripts

--- install/01-dependencies.sh
+++
@@ -1,2 +0,0 @@
-sudo dnf install -y alacritty bat curl fzf git tig htop jq make mc neovim nodejs openssl-devel zlib-devel perl-App-cpanminus starship the_silver_searcher tmux stow
-flatpak install -y com.bitwarden.desktop com.todoist.Todoist io.typora.Typora md.obsidian.Obsidian org.telegram.desktop us.zoom.Zoom
