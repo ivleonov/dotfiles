71fd82a4b40e1d78097ab6f32089edde51eb3d13
Ivan Leonov <ivanleonov@gmail.com>
2024-07-13 13:56:39 +0300

    [NeoVim] Changes ordering of telescope for search in current buffer
    If two lines are equally relevant they will be ordered by line number

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -6,6 +6,23 @@ local nmap = function(keys, func, desc)
   vim.keymap.set("n", keys, func, { desc = desc })
 end
 
+-- Taken from https://github.com/nvim-telescope/telescope.nvim/issues/1080#issuecomment-1592392087
+local current_buffer_fuzzy_find = function()
+  require("telescope.builtin").current_buffer_fuzzy_find({
+    tiebreak = function(entry1, entry2, prompt)
+      local start_pos1, _ = entry1.ordinal:find(prompt)
+      if start_pos1 then
+        local start_pos2, _ = entry2.ordinal:find(prompt)
+        if start_pos2 then
+          return start_pos1 > start_pos2
+        end
+      end
+      return false
+    end,
+    additional_args = { "--ignore-case", "--pcre2" },
+  })
+end
+
 local search_in_obsidian = function()
   require("telescope.builtin").find_files({cwd="$SECOND_BRAIN_DIR"})
 end
@@ -25,7 +42,7 @@ nmap("<leader>sb", builtin.buffers, "[S]earch existing [B]uffers")
 nmap("<leader>sj", builtin.jumplist, "[S]earch in [J]umplist")
 nmap("<leader>so", search_in_obsidian, "[S]earch in [O]bsidian")
 nmap("<leader>?", builtin.oldfiles, "[?] Find recently opened files")
-nmap("<leader>/", builtin.current_buffer_fuzzy_find, "[/] Fuzzily search in current buffer")
+nmap("<leader>/", current_buffer_fuzzy_find, "[/] Fuzzily search in current buffer")
 
 -- Enable telescope fzf native, if installed
 pcall(require("telescope").load_extension, "fzf")
