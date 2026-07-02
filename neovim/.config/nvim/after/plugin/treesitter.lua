df9d9552c62a0bc5f6ae1b04fbeddca8443d4dfb
Ivan Leonov <ivan.leonov.ext@qonto.com>
2026-07-02 14:48:11 +0300

    [NeoVim] Disables treesitter-based folding

--- neovim/.config/nvim/after/plugin/treesitter.lua
+++ neovim/.config/nvim/after/plugin/treesitter.lua
@@ -24,6 +24,8 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- treesitter based folding
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldenable = false
+-- https://github.com/nvim-treesitter/nvim-treesitter/tree/master#folding
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- vim.opt.foldenable = false
